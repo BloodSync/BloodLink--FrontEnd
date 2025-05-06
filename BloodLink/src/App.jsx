0855d117842cad2d291efa9abb5d781341c0e9d9
Klintar-afk <jonaspera741@gmail.com>
2025-05-06 19:09:52 -0300

    :sparkles: Adiciona componente de entrada para email e senha

--- BloodLink/src/App.jsx
+++ BloodLink/src/App.jsx
@@ -1,11 +1,12 @@
 import { Button } from './components/Button/Button'
 import UserProfile from './components/Perfil/UserProfile'
 import './App.css'
-
+import Input from './components/Input/Input'
 function App() {
 
   return (
     <>
+<Input/>
  <div style={{ padding: "40px", display: "flex", gap: "20px" }}>
       <Button type="primary" onClick={() => alert("Doar Agora!")}>
         Doar Agora
---
+++ BloodLink/src/components/Input/Input.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+function Input(){
+    return(
+        <>
+        <div>
+        <label For="">Digite Email<input placeholder="Exemplo@gmail.com" type="email"/></label>
+        </div>
+         <br/>
+
+         <div>
+         <label For="">Digite sua senha<input placeholder="*****" type="password"/></label>
+         </div>
+        </>
+    )
+}
+export default Input
