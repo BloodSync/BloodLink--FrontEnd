662de0a3ab7897cc51e61a6f38058417b8b6b048
Ingrid <Ingridmalik668@hotmail.com>
2025-06-21 20:44:09 -0300

    ✅ Update

--- BloodLink/src/components/Footer/Footer.jsx
+++ BloodLink/src/components/Footer/Footer.jsx
@@ -1,20 +1,20 @@
 import "../Footer/Footer.css";
 import logo from '../../assets/logo2.png';
-import { Link } from 'react-router-dom'; //router
-
+import footerVector from '../../assets/footer-vector.png';
+import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
         <>
-            <footer class="footer">
-                <div class="footer-container">
-
+            <img src={footerVector} alt="Curva do rodapé" className="footer-vector" />
+            <footer className="footer">
+                <div className="footer-container">
                     <div className="footer-brand">
                         <Link to="/" className="footer-logo-link">
                             <div className="logo-container-footer">
-                                <img src="/public/img/logo.png" alt="BloodLink Logo" className="footer-logo" />
+                                <img src={logo} alt="BloodLink Logo" className="footer-logo" />
                                 <h2 className="footer-text">
-                                    <span className="blood-red">Blood</span><span className="link-black">Link</span>
+                                    <span className="blood-red">Blood</span><span className="link-highlight">Link</span>
                                 </h2>
                             </div>
                         </Link>
@@ -22,48 +22,47 @@ function Footer() {
                             Conectamos doadores a quem mais precisa, tornando a doação de sangue acessível e engajadora.
                         </p>
                     </div>
-                    
-                    <div class="footer-content">
-                        <div class="footer-column">
+
+                    <div className="footer-content">
+                        <div className="footer-column">
                             <h4>Projeto BloodLink</h4>
                             <ul>
-                                <Link to="/AboutUs" >
-                                    <li><a>Sobre nós </a></li>
-                                </Link>
+                                <li>   <Link to="/AboutUs"> Sobre nós </Link></li>
+
                                 <li><a href="#">Apoie o BloodLink</a></li>
-                                 <Link to="/Terms">
-                                <li><a>Termos e condições</a></li>
-                                <li><a>Privacidade</a></li>
-                                 </Link>
+
+                                <li><Link to="/Terms"> Termos e condições </Link> </li>
+
+                                <li><a href="https://heyzine.com/flip-book/216439937c.html">Dossiê</a></li>
                             </ul>
                         </div>
 
-                        <div class="footer-column">
+                        <div className="footer-column">
                             <h4>Central de informações</h4>
                             <ul>
-                                <li><a href="#">Como funciona a doação?</a></li>
-                                <li><a href="#">Requisitos para doar</a></li>
-                                <li><a href="#">Quem Precisa de sangue?</a></li>
-                                <li><a href="#">Desafios e recompensas</a></li>
+                                <Link to="/InformationCenter">
+                                    <li><a href="#">Requisitos para doar</a></li>
+                                    <li><a href="#">Como funciona a BloodLink</a></li>
+                                </Link>
                             </ul>
                         </div>
 
-                        <div class="footer-column">
+                        <div className="footer-column">
                             <h4>Fale com a gente</h4>
                             <ul>
-                                <li><a href="#">Email: doei@bloodsync.com</a></li>
-                                <li><a href="#">SAC: (11) 99999-0000</a></li>
-                                <li><a href="#">WhatsApp: (11) 98888-1234</a></li>
+                                    <li><a href="mailto:doei@bloodsync.com">Email: doei@bloodsync.com</a></li>
                             </ul>
                         </div>
                     </div>
                 </div>
                 <hr />
-                <div class="footer-bottom">
+                <div className="footer-bottom">
                     <p>&copy; 2025 BloodLink. Todos os direitos reservados.</p>
                 </div>
-            </footer>
+
+            </footer >
         </>
     );
 }
+
 export default Footer;
