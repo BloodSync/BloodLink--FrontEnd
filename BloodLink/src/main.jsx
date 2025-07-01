839aa146a9dd55bd3d3c8d8289dc2f21640045f3
Guilhermepfor <pequatroguy@gmail.com>
2025-07-01 14:54:11 -0300

    📝Login google

--- BloodLink/src/main.jsx
+++ BloodLink/src/main.jsx
@@ -1,6 +1,14 @@
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { StrictMode } from "react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+const CLIENT_ID = "812350191038-g3t0ip5da4q0p848fbsgo2j04246p1iu.apps.googleusercontent.com"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <GoogleOAuthProvider clientId={CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
+  </StrictMode>
 );
--- BloodLink/src/pages/Cadastro/Register.jsx
+++ BloodLink/src/pages/Cadastro/Register.jsx
@@ -1,99 +1,81 @@
-import './Register.css';
-import logoImage from '../../assets/logo2.png'; // seu logo
-import logoGoogle from '../../assets/Logo-Google.png';
-import { Link } from 'react-router-dom'; //router
+import "./Register.css";
+import logoImage from "../../assets/logo2.png"; // seu logo
+import logoGoogle from "../../assets/Logo-Google.png";
+import { Link, useNavigate} from "react-router-dom"; //router
+import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
 
 function Cadastro() {
-    return (
-        <div className="RegisterBg">
-            <div className="Register">
-                <Link to="/">
-                    <div className="logoRegister">
-                        <img src={logoImage} alt="Logo BloodLink" />
-                        <h1>
-                            <span>Blood</span> <span>Link</span>
-                        </h1>
-                    </div>
-                </Link>
+  const navigate = useNavigate();
 
-                <h2>Registrar-se</h2>
+  return (
+    <div className="RegisterBg">
+      <div className="Register">
+        <Link to="/">
+          <div className="logoRegister">
+            <img src={logoImage} alt="Logo BloodLink" />
+            <h1>
+              <span>Blood</span> <span>Link</span>
+            </h1>
+          </div>
+        </Link>
 
-                <form className='label-info-register'>
+        <h2>Registrar-se</h2>
 
-                    <label>E-mail</label>
-                    <input
-                        type="email"
-                        name="email"
-                        placeholder="Exemplo@hotmail.com"
-                    />
+        <form className="label-info-register">
+          <label>E-mail</label>
+          <input type="email" name="email" placeholder="Exemplo@hotmail.com" />
 
-                    <label>Nome completo</label>
-                    <input
-                        type="text"
-                        name="nome"
-                        placeholder="Digite seu nome"
-                    />
+          <label>Nome completo</label>
+          <input type="text" name="nome" placeholder="Digite seu nome" />
 
-                    <div className="inline-inputs">
-
-                        <div className="input-group">
-                            <label>Data de nascimento</label>
-                            <input
-                                type="date"
-                                name="nascimento"
-                                placeholder="00/00/0000"
-                            />
-                        </div>
-
-                        <div>
-
-
-                            <label>Nome de Usuário</label>
-                            <div className="username-wrapper">
-                                <input
-                                    type="text"
-                                    name="username"
-                                    placeholder="@Exemplo"
-                                />
-                            </div>
-
-                        </div>
-                    </div>
-
-                    <label>Senha</label>
-                    <input
-                        type="password"
-                        name="senha"
-                        placeholder="Digite sua senha"
-                    />
-
-                    <label>Confirme a senha</label>
-                    <input
-                        type="password"
-                        name="confirmarSenha"
-                        placeholder="Digite sua senha novamente"
-                    />
-
-                    <div class="containerGoogle">
-                        <button className="register-Google-button" type="button">
-                            <img src={logoGoogle} alt="Logo Google" />
-                            <span>Registrar com Google</span>
-                        </button>
-                    </div>
-
-                    <Link to="/cadastroinfo">
-                        <button type="submit" className="continuar-btn">
-                            Continuar
-                        </button>
-                    </Link>
-                    <a href="/login" className="voltar-home">
-                        &lt; Voltar para entrar
-                    </a>
-
-                </form>
+          <div className="inline-inputs">
+            <div className="input-group">
+              <label>Data de nascimento</label>
+              <input type="date" name="nascimento" placeholder="00/00/0000" />
             </div>
-        </div>
-    );
+
+            <div>
+              <label>Nome de Usuário</label>
+              <div className="username-wrapper">
+                <input type="text" name="username" placeholder="@Exemplo" />
+              </div>
+            </div>
+          </div>
+
+          <label>Senha</label>
+          <input type="password" name="senha" placeholder="Digite sua senha" />
+
+          <label>Confirme a senha</label>
+          <input
+            type="password"
+            name="confirmarSenha"
+            placeholder="Digite sua senha novamente"
+          />
+
+          <div class="containerGoogle">
+              <GoogleLogin
+                onSuccess={(credentialResponse) => {
+                  console.log(credentialResponse);
+                  console.log(jwtDecode(credentialResponse.credential)); // Login com o google
+                  navigate("/cadastroinfo");
+                }}
+                onError={() => console.log("Login failed")}
+              />
+          </div>
+
+          <Link to="/cadastroinfo">
+            <button type="submit" className="continuar-btn">
+              Continuar
+            </button>
+          </Link>
+          <a href="/login" className="voltar-home">
+            &lt; Voltar para entrar
+          </a>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Cadastro;
